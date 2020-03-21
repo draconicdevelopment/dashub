941a1b849eace91defff4ba4213c4a143c408b57
Martin Kaněra <marti.kanera@gmail.com>
2020-03-21 22:36:33 +0100

    feat(dashboard): added drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,63 @@
 <template>
   <div>
+    <div class="nav">
+      <div class="logo-container">
+        <img
+          class="logo"
+          src="../static/img/LightIcon.svg"
+          width="106"
+          height="106"
+          color="red"
+        />
+      </div>
+      <ul class="navbar-nav">
+        <li class="nav-item"></li>
+      </ul>
+    </div>
     <nuxt />
   </div>
 </template>
+
+<style>
+:root {
+  --primary-bg-color: #202020;
+  --secondary-bg-color: #303030;
+}
+</style>
+
+<style lang="sass" scoped>
+.nav
+  width: 128px
+  height: 96%
+  top: 2%
+  background-color: var(--secondary-bg-color)
+  position: fixed
+  box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.25);
+  border-radius: 0px 25px 25px 0px;
+  transition: ease 300ms
+
+  &:hover
+    width: 360px
+
+    .logo-container
+      justify-content: flex-end
+
+
+  .logo-container
+    background: red
+    display: flex
+    height: 128px
+    background: linear-gradient(135deg, #8A2BE2 0%, #E22BD0 100%)
+    box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.25)
+    border-radius: 0px 25px 25px 0px;
+    align-items: center
+    justify-content: center
+
+  .navbar-nav
+    padding: 0
+    margin: 0
+    list-style-type: none
+    display: flex
+    flex-direction: column
+    align-items: center
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-    <p>ahoj</p>
+    <div class="board"></div>
   </div>
 </template>
+
+<style lang="sass">
+body
+  margin: 0
+  background: var(--primary-bg-color)
+
+.board
+  display: block
+  margin-left: 192px
+  margin-right: 64px
+</style>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
