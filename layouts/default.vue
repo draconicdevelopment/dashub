e627d7e33124811f69b0d7fd419e15add218a285
Martin Kaněra <marti.kanera@gmail.com>
2020-03-22 18:02:56 +0100

    feat(dashboard): advanced drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <div class="nav">
+    <transition name="fade">
+      <div v-if="state.display" class="blur"></div>
+    </transition>
+    <div
+      class="nav"
+      @mouseover="state.display = true"
+      @mouseleave="state.display = false"
+    >
       <div class="logo-container">
         <img
           class="logo"
           src="../static/img/LightIcon.svg"
           width="106"
           height="106"
-          color="red"
         />
       </div>
       <ul class="navbar-nav">
@@ -18,40 +24,43 @@
   </div>
 </template>
 
-<style>
-:root {
-  --primary-bg-color: #202020;
-  --secondary-bg-color: #303030;
-}
-</style>
-
 <style lang="sass" scoped>
+.blur
+  width: 100%
+  height: 100%
+  top: 0
+  left: 0
+  position: fixed
+  backdrop-filter: saturate(100%) blur(12px)
+
+.fade-enter-active, .fade-leave-active
+  transition: opacity .15s
+
+.fade-enter, .fade-leave-to
+  opacity: 0;
+
 .nav
   width: 128px
   height: 96%
   top: 2%
   background-color: var(--secondary-bg-color)
   position: fixed
-  box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.25);
-  border-radius: 0px 25px 25px 0px;
-  transition: ease 300ms
+  box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.25)
+  border-radius: 0px 25px 25px 0px
+  transition: ease .15s
 
   &:hover
     width: 360px
 
-    .logo-container
-      justify-content: flex-end
-
-
   .logo-container
-    background: red
     display: flex
     height: 128px
     background: linear-gradient(135deg, #8A2BE2 0%, #E22BD0 100%)
     box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.25)
-    border-radius: 0px 25px 25px 0px;
+    border-radius: 0px 25px 25px 0px
     align-items: center
     justify-content: center
+    transition: ease 300ms
 
   .navbar-nav
     padding: 0
@@ -61,3 +70,19 @@
     flex-direction: column
     align-items: center
 </style>
+
+<script lang="ts">
+import { defineComponent, reactive } from '@vue/composition-api';
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      display: false,
+    });
+
+    return {
+      state,
+    };
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <div class="board"></div>
+    <div class="board"><img src="../static/img/rd.png" width="180" /></div>
   </div>
 </template>
+
+<style>
+:root {
+  --primary-bg-color: #202020;
+  --secondary-bg-color: #303030;
+}
+</style>
 
 <style lang="sass">
 body
@@ -11,8 +18,12 @@ body
 
 .board
   display: block
+  height: 100vh
   margin-left: 192px
   margin-right: 64px
+  display: flex
+  justify-content: center
+  align-items: center
 </style>
 
 <script lang="ts">
