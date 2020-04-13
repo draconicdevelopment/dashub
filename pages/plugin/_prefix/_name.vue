c4a3fa99b190dcc5270a2219b22063e1dbfb901d
Martin Kaněra <marti.kanera@gmail.com>
2020-04-13 21:36:22 +0200

    feat(plugins): loading html from dash_plugins

--- pages/plugin/_prefix/_name.vue
+++ pages/plugin/_prefix/_name.vue
@@ -1,40 +1,55 @@
 <template>
-  <client-only>
+  <div>
     <iframe
       style="margin-left: 200px"
       width="80%"
       height="500px"
       frameborder="0"
-      src="../../../dash_plugins/dashub/graph/main.html"
+      :srcdoc="html"
     ></iframe>
-  </client-only>
+  </div>
 </template>
 
-<style lang="sass"></style>
-
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import fs from 'fs';
+import { defineComponent, ref } from '@vue/composition-api';
 import { Context } from '@nuxt/types';
 
 export default defineComponent({
   validate(ctx: Context): boolean {
-    const fs = require('fs');
     const params = ctx.route.params;
 
-    // console.log(ctx.route.fullPath.split('/'));
-
-    if (params?.prefix) {
-      return fs.statSync(`dash_plugins/${params.prefix}/${params.name}`);
+    if (params?.name) {
+      return !!fs.statSync(`dash_plugins/${params.prefix}/${params.name}`);
     } else {
-      return fs.statSync(`dash_plugins/unclassified/${params.name}`);
+      return !!fs.statSync(`dash_plugins/unclassified/${params.prefix}`);
     }
   },
   setup(_, ctx) {
     const params = ctx.root.$route.params;
+    // eslint-disable-next-line prefer-const
+    let html = ref('');
+
+    if (process.server) {
+      const path =
+        'dash_plugins/' +
+        (params?.name
+          ? `${params.prefix}/${params.name}`
+          : `unclassified/${params.prefix}`) +
+        '/main.html';
+
+      html.value = fs.readFileSync(path, 'utf8');
+    }
 
     return {
-      params,
+      html,
     };
   },
 });
 </script>
+
+<style lang="sass">
+iframe body
+  margin: 0
+  padding: 0
+</style>
