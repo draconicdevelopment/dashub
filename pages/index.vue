d0220aeff4dc7e28c960bed79c3a62e091dec474
Martin Kaněra <marti.kanera@gmail.com>
2020-05-04 13:49:20 +0200

    feat(login): mobile responsible

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="container">
     <div class="card">
-      <div class="logo"></div>
-      <form>
-        <div class="input-container">
-          <input placeholder="email" type="email" />
-          <input placeholder="password" type="password" />
-        </div>
-        <button class="btn-login">LOG IN</button>
-      </form>
+      <div class="card-content">
+        <div class="logo"></div>
+        <form>
+          <div class="input-container">
+            <input placeholder="email" type="email" />
+            <input placeholder="password" type="password" />
+          </div>
+          <button class="btn-login">LOG IN</button>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -21,78 +23,106 @@
   align-items: center
 
   .card
-    display: flex
-    flex-direction: column
-    align-items: center
-    padding: 24px
     background-color: var(--secondary-bg-color)
     width: 400px
     height: 450px
     box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.25)
     border-radius: 25px
+    padding: 24px
 
-    .logo
-      background-image: url("../static/img/dashub.png")
-      background-repeat: no-repeat
-      background-position: center center
-      width: 500px
-      height: 90px
-      transform: scale(0.7)
-      margin-top: 5px
-
-    form
-      flex-grow: 2
+    .card-content
       display: flex
       flex-direction: column
-      justify-content: space-between
-      margin-top: 80px
-      width: 88%
+      align-items: center
+      width: 100%
+      height: 100%
 
-      .input-container
-        width: 100%
-        height: 100px
+      .logo
+        background-image: url("../static/img/dashub.png")
+        background-repeat: no-repeat
+        background-position: center center
+        width: 500px
+        height: 90px
+        transform: scale(0.7)
+        margin-top: 5px
+
+      form
+        flex-grow: 2
         display: flex
         flex-direction: column
         justify-content: space-between
+        margin-top: 80px
+        width: 88%
 
-        input
-          background: transparent
-          padding: 6px
-          border: none
-          border-bottom: 1.5px solid #939393
-          color: #939393
-          font-size: 16px
-          line-height: 1.5
-          transition: border ease-in-out .4s
+        .input-container
+          width: 100%
+          height: 100px
+          display: flex
+          flex-direction: column
+          justify-content: space-between
 
-          &:focus
-            outline: none
-            border-bottom-color: #FF007A
-
-          &::placeholder
+          input
+            background: transparent
+            padding: 6px
+            border: none
+            border-bottom: 1.5px solid #939393
             color: #939393
             font-size: 16px
+            line-height: 1.5
+            transition: border ease-in-out .4s
 
-      .btn-login
-        width: 100%
-        padding: 15px
+            &:focus
+              outline: none
+              border-bottom-color: #FF007A
 
-        margin-bottom: 30px
-        border-radius: 25px
-        border: none
-        color: white
-        font-weight: bold
-        background: linear-gradient(90deg, #FF007A 0%, rgba(255, 0, 199, 0.8) 100%)
-        background-color: #FF007A
-        box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.25);
-        cursor: pointer
-        transition: background ease-in-out .25s
-        transition: box-shadow ease-in-out .25s
+            &::placeholder
+              color: #939393
+              font-size: 16px
 
-        &:hover
-          background: transparent
-          background-color: transparent
-          box-shadow: 0px 8px 16px rgba(255, 0, 199, 0.25);
-          padding: 13px
-          border: 2px solid #FF007A
+        .btn-login
+          width: 100%
+          padding: 15px
+
+          margin-bottom: 30px
+          border-radius: 25px
+          border: none
+          color: white
+          font-weight: bold
+          background: linear-gradient(90deg, #FF007A 0%, rgba(255, 0, 199, 0.8) 100%)
+          background-color: #FF007A
+          box-shadow: 8px 8px 16px rgba(0, 0, 0, 0.25);
+          cursor: pointer
+          transition: background ease-in-out .25s
+          transition: box-shadow ease-in-out .25s
+
+          &:hover
+            background: transparent
+            background-color: transparent
+            box-shadow: 0px 8px 16px rgba(255, 0, 199, 0.25);
+            padding: 13px
+            border: 2px solid #FF007A
+</style>
+
+<style>
+@media only screen and (max-width: 480px) {
+  .card {
+    width: 100vw !important;
+    height: 100vh !important;
+    padding: 0 !important;
+    border-radius: 0 !important;
+  }
+
+  .card-content {
+    height: 60% !important;
+    margin-top: 80px !important;
+  }
+
+  .logo {
+    transform: scale(0.55) !important;
+  }
+
+  form {
+    width: 76% !important;
+  }
+}
 </style>
