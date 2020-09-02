2f0cd62d74d8e74782e4e8f5af6e41e6c421b519
Martin Kaněra <50818204+MartinKanera@users.noreply.github.com>
2020-09-02 18:59:39 +0200

    Update firebase.ts

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -2,16 +2,7 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBp95Odjc3I3WG7k4DGSSqb9LZC1FKExZQ',
-  authDomain: 'dashub-ecfb7.firebaseapp.com',
-  databaseURL: 'https://dashub-ecfb7.firebaseio.com',
-  projectId: 'dashub-ecfb7',
-  storageBucket: 'dashub-ecfb7.appspot.com',
-  messagingSenderId: '874351010489',
-  appId: '1:874351010489:web:e54fc45f23d51834923168',
-  measurementId: 'G-3F2M69JSHM',
-};
+const firebaseConfig = {};
 
 export const provideFirebase = () => {
   if (firebase.apps.some((app) => app?.name === 'default')) {
